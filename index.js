88112ec0a51485ed763caac4bcd2a170ad04ee06
Jeff Racaniello <jeffrac@gmail.com>
2021-03-20 20:01:26 -0500

    deStijl project

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
-// (1)- selecting svg path
-const svg = document.querySelector('.background svg path')
-// (2)- storing the svg path
-const svgPath = svg.getAttribute('d')
-// (3)- saving the new svg path
-const svgNewPath =
-	'M-446,1199C-446,1199 -843,362.333 -744,297C-645,231.667 -598,-159.333 -499,-216C-400.64,-272.3 4.841,-158.333 158,-246C523.479,-455.196 796.667,-84.333 895,-172C993.333,-259.667 1281.67,35.667 1392,-52C1502.33,-139.667 2054,-52 2054,-52L2054,1206L-446,1199Z'
+// (1)- selecting the h1. h2, button
+const h1 = document.querySelector('main h1');
+const h2 = document.querySelector('main h2');
+const btn = document.querySelector('main button');
 
-// (4)- selecting button, back button, main intro
-const btn = document.querySelector('section button')
-const btnBack = document.querySelector('header button')
-const backArrowSvg = document.querySelector('.btn-arrow svg path')
-const main = document.querySelector('main')
+// (2)- selecting all the div inside section
+const boxes = document.querySelectorAll('section div');
 
-// (5)- selecting button, back button, main intro
-btn.addEventListener('click', () => {
-	// (6)- set the new svg path to new path
-	svg.setAttribute('d', svgNewPath)
-	// (7)- hide the next button
-	btn.classList.add('btn-fade')
+// (3)- adding event listener
+btn.addEventListener('mouseover', ()=> {
+    // (4)- adding all the active classes
+    h1.classList.add('heading-active');
+    h2.classList.add('heading-active');
+    btn.classList.add('btn-active');
 
-	// (9)- hide the next button
-	setTimeout(() => {
-		// (8)- hide the next button
-		main.classList.add('main-active')
-		backArrowSvg.setAttribute('fill', '#fff')
-	}, 400)
+    // (5)- loop over divs
+    boxes.forEach((j) => {
+        j.style.cssText = `transform: scale(0);`;
+        j.classList.add('scale-up');
+        j.classList.remove('scale-down');
+    }) 
 })
 
-// (6)- add for back button
-btnBack.addEventListener('click', () => {
-	svg.setAttribute('d', svgPath)
-	main.classList.remove('main-active')
-
-	backArrowSvg.setAttribute('fill', '#000')
-
-	setTimeout(() => {
-		btn.classList.remove('btn-fade')
-	}, 800)
+// (6)- reverse the mouse out
+btn.addEventListener('mouseout', () => {
+    h1.classList.remove('heading-active');
+    h2.classList.remove('heading-active');
+    btn.classList.remove('btn-active');
+    
+    boxes.forEach((j) => {
+        j.style.cssText = `transform: scale(1);`;
+        j.classList.add('scale-down');
+        j.classList.remove('scale-up');
+    })
 })
